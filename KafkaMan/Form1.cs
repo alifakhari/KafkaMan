c4b6291f7b56efd89abc703652ad7b393658e5c9
ALI <ali@tamin.local>
2023-12-24 11:53:30 +0330

    consumer added

--- KafkaMan/Form1.cs
+++ KafkaMan/Form1.cs
@@ -16,14 +16,34 @@ namespace KafkaMan
 
 
         #region Functions
+
+        //public static List<TopicPartition> GetTopicPartitions(string bootstrapServers, string topicValue)
+        public List<TopicPartition> GetTopicPartitions(string topicValue)
+        {
+            //AdminClientConfig adminConfig = new AdminClientConfig { BootstrapServers = bootstrapServers };
+            //AdminClientConfig adminConfig = new AdminClientConfig { BootstrapServers = bootstrapServers };
+
+            using (var adminClient = new AdminClientBuilder(adminConfig).Build())
+            {
+                var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+                TopicMetadata? topicMetadata = meta.Topics.SingleOrDefault(t => topicValue.Equals(t.Topic));
+                if (topicMetadata != null)
+                {
+                    return topicMetadata.Partitions
+                        .Select(x => new TopicPartition(topicMetadata.Topic, x.PartitionId))
+                        .ToList();
+                }
+            }
+            return new List<TopicPartition>();
+        }
+
+
         public void func_getTopics()
         {
-
             using (var adminClient = new AdminClientBuilder(adminConfig).Build())
             {
                 try
                 {
-
                     var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(10));
                     var topicsMetadata = metadata.Topics;
                     var topicNames = metadata.Topics.Select(a => a.Topic).ToList();
@@ -34,13 +54,42 @@ namespace KafkaMan
 
                     foreach (string topic in topicNames)
                     {
+                        var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+                        //var count = 0;
+                        TopicMetadata? topicMetadata = meta.Topics.SingleOrDefault(t => topic.Equals(t.Topic));
+
+                        //if (topicMetadata != null)
+                        //{
+                        //var count =  topicMetadata.Partitions
+                        //    .Select(x => new TopicPartition(topicMetadata.Topic, x.PartitionId))
+                        //    .ToList();
+                        //}
+
+                        ConsumerConfig config = new ConsumerConfig
+                        {
+                            BootstrapServers = "192.168.189.128:9092",
+                            GroupId = "1",
+                            AutoOffsetReset = AutoOffsetReset.Earliest,
+                        };
+                        ConsumerBuilder<Null, string> builder = new ConsumerBuilder<Null, string>(config);
+                        //builder.SetValueDeserializer(_kafkaConverter);
+
+                        IConsumer<Null, string> consumer = builder.Build();
+
+                        //List<TopicPartition> partitions = GetTopicPartitions(BootstrapServers, topic);
+                        List<TopicPartition> partitions = GetTopicPartitions(topic);
+                        TopicPartition firstPartition = partitions.First();
+
+                        WatermarkOffsets watermarkOffsets = consumer.QueryWatermarkOffsets(firstPartition, TimeSpan.FromSeconds(10));
+                        long total = watermarkOffsets.High - watermarkOffsets.Low;
+                        System.Console.WriteLine(total);
+
                         lstTopicList.Items.Add(topic);
                         cboTopicProducer.Items.Add(topic);
                         cbotopicConsumer.Items.Add(topic);
 
                     }
                     lblLastSync.Text = "Last sync: " + DateTime.Now;
-
 
                 }
                 catch (Exception ex)
@@ -51,6 +100,43 @@ namespace KafkaMan
                 }
             }
         }
+
+        //public void func_getMessageCountinTopic()
+        //{
+
+        //    using (var adminClient = new AdminClientBuilder(adminConfig).Build())
+        //    {
+        //        var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+        //        TopicMetadata? topicMetadata = meta.Topics.SingleOrDefault(t => topicValue.Equals(t.Topic));
+        //        if (topicMetadata != null)
+        //        {
+        //            return topicMetadata.Partitions
+        //                .Select(x => new TopicPartition(topicMetadata.Topic, x.PartitionId))
+        //                .ToList();
+        //        }
+        //    }
+        //    return new List<TopicPartition>();
+
+
+            //ConsumerConfig config = new ConsumerConfig
+            //{
+            //    //BootstrapServers = _bootstrapServers,
+            //    //GroupId = _groupId,
+            //    AutoOffsetReset = AutoOffsetReset.Earliest,
+            //};
+            //ConsumerBuilder<Null, string> builder = new ConsumerBuilder<Null, string>(config);
+            ////builder.SetValueDeserializer(_kafkaConverter);
+
+            //IConsumer<Null, string> consumer = builder.Build();
+
+            //List<TopicPartition> partitions = KafkaAdmin.GetTopicPartitions(, _topic);
+            //TopicPartition firstPartition = partitions.First();
+
+            //WatermarkOffsets watermarkOffsets = consumer.QueryWatermarkOffsets(firstPartition, TimeSpan.FromSeconds(10));
+            //long total = watermarkOffsets.High - watermarkOffsets.Low;
+            //System.Console.WriteLine(total);
+
+        //}
         #endregion Functions
 
 
